58f4252cf924fa3fcd79d60024195d23acd757c2
Efras77 <efra10_efraim@hotmail.com>
2023-08-11 10:40:05 -0300

    eu sou o cara

---
+++ teste/atividade1/verificaNumeroPrimo.java
@@ -0,0 +1,17 @@
+package atividade1;
+
+/**
+ * verificaNumeroPrimo
+ */
+public class verificaNumeroPrimo {
+
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        verificarNumeroPrimo(12345);
+    }
+
+    public static void verificarNumeroPrimo(Integer n) {
+
+    }
+
+}
