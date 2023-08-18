104d92f9383c93d87eae41929e58d1875d060bf1
Efraim <efra10_efraim@hotmail.com>
2023-08-17 23:57:28 -0300

    completo

--- atividade1/verificaNumeroPrimo.java
+++ atividade1/verificaNumeroPrimo.java
@@ -1,31 +1,28 @@
 package atividade1;
 
-/**
- * verificaNumeroPrimo
- */
+
 public class verificaNumeroPrimo {
 
     public static void main(String args[]) {
-        verificarNumeroPrimo(179); /* digitar número primo */
+        System.out.println(verificarNumeroPrimo(13));
     }
 
-    public static void verificarNumeroPrimo(Integer n) {
-        /* criar variável inteira */
+    /* verificaNumeroPrimo */
+    public static int verificarNumeroPrimo(Integer n) {
+        int num_dividers = 2;
+        int is_prime = 0;
 
-        if (n < 2) {
-            System.out.println("0");
-        } else {
-            int qtdDivisores = 2;
-            for (int i = 2; i < n/2; i++) {
+        if (n >= 2) {
+            for (int i = 2; i < n; i++) {
                 if (n % i == 0) {
-                    qtdDivisores++;
+                    num_dividers++;
                 }
             }
-            if (qtdDivisores <= 2) {
-                System.out.println("1");
-            } else {
-                System.out.println("0");
+            if (num_dividers <= 2) {
+                is_prime = 1;
             }
         }
+
+        return is_prime;
     }
 }
